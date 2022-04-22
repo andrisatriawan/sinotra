5562ac9450117a53958851f53f004c9b205a72d1
Andri Satriawan <satria.citieng@gmail.com>
2022-04-22 15:03:45 +0700

    base

---
+++ application/controllers/Test.php
@@ -0,0 +1,14 @@
+<?php
+defined('BASEPATH') or exit('No direct script access allowed');
+
+class Test extends CI_Controller
+{
+    public function index()
+    {
+        echo 'ini test dan index';
+    }
+    public function coba()
+    {
+        echo 'ini test dan coba';
+    }
+}
